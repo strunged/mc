8c2682b735b043b97e433b3cbdfa7ae4fd73015d
alpha <56209673+aIphax@users.noreply.github.com>
2022-03-03 05:04:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const axios = require("axios")
 
 app.use("/public", express.static(__dirname + "/public"))
 
+app.get("/users", async function (req, res) {
+    try {
+        const ign = req.query.ign || 'Nigerian'
+        const inocentMTCHATOMANN = await (await axios.get('https://api.mojang.com/users/profiles/minecraft/' + ign)).data
+        const mtmtmtMTCHATOMANN = await (await axios.get('https://api.gapple.pw/status/' + inocentMTCHATOMANN.id)).data
+        
+        const skin = `https://crafatar.com/renders/body/${inocentMTCHATOMANN.id}`
+        let migrada = (mtmtmtMTCHATOMANN.status.startsWith('migrated') || mtmtmtMTCHATOMANN.status.startsWith('msa')) ? 'Microsoft' : 'Mojang'
+
+        res.render("index.ejs", { skin, name: inocentMTCHATOMANN.name, migrada })
+    } catch (e) {
+        res.redirect('/users')
+    }
+})
 
 
-
-app.listen(80)
+app.listen(process.env.PORT)
